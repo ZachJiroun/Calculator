61cc711e50fec69fb61982013f661b436556e057
ZachJiroun <zjiroun@gmail.com>
2016-02-09 23:47:04 -0500

    added app icon

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        clearButton.titleLabel?.textAlignment = NSTextAlignment.Center
+        clearButton.titleLabel!.textAlignment = NSTextAlignment.Center
     }
     
     override func didReceiveMemoryWarning() {
